a57a78eccbcb49be721cf82634a8aa0d0989c95c
ekaterina533 <ekaterinahrcnk@gmail.com>
2021-06-04 01:24:43 +0300

    создание

---
+++ инд1.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Решить индивидуальное задание лабораторной работы 6,
+# оформив каждую команду в виде отдельной функции.
+
+import sys
+
+if __name__ == '__main__':
+
+    def addtobase():
+        name = input("Фамилия, Имя? ")
+        number = input("Номер телефона? ")
+        print("Дата рождения:")
+        day = input("День")
+        month = input("Месяц")
+        year = input("Год")
+        bases = {
+            'name': name,
+            'number': number,
+            'day': day,
+            'month': month,
+            'year': year
+        }
+        base.append(bases)
+    def sortbase():
+        if len(base) > 1:
+            base.sort(key=lambda item: item.get('name', ''))
+
+    def formatbase():
+     line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+        '-' * 4,
+        '-' * 30,
+        '-' * 20,
+        '-' * 8
+     )
+     print(line)
+     print(
+        '| {:^4} | {:^30} | {:^20} | {:^8} |'.format(
+            "№",
+            "Ф.И.",
+            "Номер телефона",
+            "Дата рождения"
+        )
+     )
+     print(line)
+     for idx, bases in enumerate(base, 1):
+        print(
+            '| {:>4} | {:<30} | {:<20} | {:>8} |'.format(
+                idx,
+                bases.get('name', ''),
+                bases.get('number', ''),
+                bases.get('month', 0)
+            )
+        )
+     print(line)
+    def selectbase():
+        parts = command.split(' ', maxsplit=2)
+        sel = (parts[1])
+        count = 0
+        for bases in base:
+            if bases.get('month') == sel:
+                count += 1
+                print(
+                    '{:>4}: {}'.format(count, bases.get('name', ''))
+                )
+        if count == 0:
+            print("В этом месяце ни у кого нет дня рождения.")
+    def help():
+        print("Список команд:\n")
+        print("add - добавить работника;")
+        print("list - вывести список работников;")
+        print("select <месяц> - запросить людей, у которых в этом месяце день рождения;")
+        print("help - отобразить справку;")
+        print("exit - завершить работу с программой.")
+    def error():
+        print(f"Неизвестная команда {command}", file=sys.stderr)
+
+    base = []
+    while True:
+     command = input(">>> ").lower()
+     if command == 'exit':
+        break
+     elif command == 'add':
+        addtobase()
+        sortbase()
+     elif command == 'list':
+        formatbase()
+     elif command.startswith('select '):
+         selectbase()
+     elif command == 'help':
+         help()
+     else:
+        error()
+
+
